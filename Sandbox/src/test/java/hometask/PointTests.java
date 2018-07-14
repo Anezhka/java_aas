618aa82236ef7422106ac289fdc16d2b5034cbd7
Anezhka <pikkuchups@gmail.com>
2018-07-14 13:39:06 +0300

    Добавлен второй тест в PointTests

--- Sandbox/src/test/java/hometask/PointTests.java
+++ Sandbox/src/test/java/hometask/PointTests.java
@@ -7,9 +7,17 @@ import org.testng.annotations.Test;
 public class PointTests {
 
   @Test
-  public void testDistance(){
+  public void testDistance() {
     Point p1 = new Point(8, 2);
     Point p2 = new Point(5, 6);
     Assert.assertEquals(p2.getDistance(p1), 5.0);
+  }
+
+
+  @Test
+  public void getDistance() {
+    Point p1 = new Point(8, 2);
+    Point p2 = new Point(5, 6);
+    Assert.assertEquals(MySecondProgram.getDistance(p1, p2), 5.0);
   }
 }
