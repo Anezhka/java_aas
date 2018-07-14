cae234fb833fad51a634a28f0cbbb36fdc69bb06
Anezhka <pikkuchups@gmail.com>
2018-07-14 12:53:51 +0300

    Создан PointTests

---
+++ Sandbox/src/test/java/hometask/PointTests.java
@@ -0,0 +1,15 @@
+package hometask;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testDistance(){
+    Point p1 = new Point(8, 2);
+    Point p2 = new Point(5, 6);
+    Assert.assertEquals(p2.getDistance(p1), 5.0);
+  }
+}
