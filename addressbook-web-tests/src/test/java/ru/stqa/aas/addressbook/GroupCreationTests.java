849db3211bbc4f5dbcd6450e5a0bae1f3aa60022
Anezhka <pikkuchups@gmail.com>
2018-07-16 00:28:12 +0300

    Добавлены TestBase и GroupDeletionTests. GroupCreationTests extends TestBase

--- addressbook-web-tests/src/test/java/ru/stqa/aas/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/aas/addressbook/GroupCreationTests.java
@@ -1,38 +1,11 @@
 package ru.stqa.aas.addressbook;
 
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
+
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
-
-public class GroupCreationTests {
-  FirefoxDriver wd;  // wd - это переменная, она является атрибутом объекта типа GroupCreationTests
-//тестовый фреймворк создает объект этого класса...
-
-  @BeforeMethod
-  public void setUp() throws Exception {  //потом выполняет метод setUp
-    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true)); //внутри метода инициализируется атрибут объекта...
-    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/group.php");
-    login("admin", "secret");  //и когда вызывается другой метов в том же самом объекте, он может этим атрибутом пользоваться.
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).sendKeys("\\undefined");
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-  }
+public class GroupCreationTests extends TestBase {
+  //тестовый фреймворк создает объект этого класса...
 
   @Test
   public void testGroupCreation() {
@@ -44,45 +17,4 @@ public class GroupCreationTests {
     returnToGroupPage();
   }
 
-  private void returnToGroupPage() {
-    wd.findElement(By.linkText("group page")).click();
-  }
-
-  private void submitGroupCreation() {
-    wd.findElement(By.name("submit")).click();
-  }
-
-  private void fillGroupForm(GroupData groupData) {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    wd.findElement(By.name("group_footer")).click();
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  private void initGroupCreation() {
-    wd.findElement(By.name("new")).click();
-  }
-
-  private void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
-  }
-
-  @AfterMethod
-  public void tearDown() {
-    wd.quit();
-  }
-
-  public static boolean isAlertPresent(FirefoxDriver wd) {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 }
