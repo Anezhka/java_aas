aaecde194b49a8d3ec982ae7d2b6c8b7f25eca2b
Anezhka <pikkuchups@gmail.com>
2018-07-18 23:16:13 +0300

    Исправлена опечатка в слове submit

--- addressbook-web-tests/src/test/java/hometask/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/hometask/tests/ContactDeletionTests.java
@@ -8,7 +8,7 @@ public class ContactDeletionTests extends TestBase {
   public void testContactDeletion(){
     app.getContactHelper().selectContact();
     app.getContactHelper().initContactDeletion();
-    app.getContactHelper().submisContactDeletion();
+    app.getContactHelper().submitContactDeletion();
     app.getContactHelper().returnToHomePage();
   }
 }
