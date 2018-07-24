e05f64622edfbf9fba088b404985188d0ab57b5a
Anezhka <pikkuchups@gmail.com>
2018-07-25 01:19:49 +0300

    Full_Реализована проверка и обеспечение выполнения предусловий

--- addressbook-web-tests/src/test/java/ru/stqa/aas/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/aas/addressbook/tests/ContactModificationTests.java
@@ -8,6 +8,9 @@ public class ContactModificationTests extends TestBase {
   @Test
   public void testContactModification() {
     app.getNavigationHelper().gotoHomePage();
+    if (! app.getContactHelper().isThereAGroup()) {
+      app.getContactHelper().createContact(new ContactData("Ivan", "Ivanovich", "Ivanov", "Ivaha", "Boss", "WSAB", "Finland, Helsinki, Tehtankatu, 35", "+358777777", "+358555555", "+358333333", "+358999999", "boss@gmail.com", "test1"), true);
+    }
     app.getContactHelper().selectContact();
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillContactForm(new ContactData("Petr", "Ivanovich", "Sidorov", "Ivaha", "Boss", "WSAB", "Finland, Helsinki, Tehtankatu, 35", "+358777777", "+358555555", "+358333333", "+358999999", "boss@gmail.com", null), false);
